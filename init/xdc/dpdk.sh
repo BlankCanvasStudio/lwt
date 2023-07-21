b3bf24b39ec81292070279a899bbd016f5fffc7d
adam <spencerstingley6@gmail.com>
2023-07-21 12:08:39 -0500

    Making build scripts more verbose

--- init/xdc/dpdk.sh
+++ init/xdc/dpdk.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+echo "Building dpdk"
+
 for target in "$@"; do
+    echo "Copying files over"
     scp ./init/loc/dpdk.sh $target:~/dpdk.sh
     scp ./nfra/bind.sh $target:~/bind.sh
+    echo "Running build"
     ssh $target 'sudo ~/dpdk.sh'
 done
 
--- init/xdc/fc.sh
+++ init/xdc/fc.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 source ./config-test.sh
-
+echo "Building fast click"
 for target in "$@"; do
+    echo "Copying files over"
     scp ./init/loc/fc.sh $target:~/fastclick.sh
     scp $click_collector_object_c_files $target:~
     scp ./nfra/bind.sh $target:~/bind.sh
+    "Running build"
     ssh $target 'sudo ~/fastclick.sh'
 done
 
