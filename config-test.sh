a900482c4c4e809b5e9303c0f9927d41269042fe
adam <spencerstingley6@gmail.com>
2023-07-21 13:43:39 -0500

    Made config easier to edit with a couple more comments

--- config-test.sh
+++ config-test.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 # This doubles as the folder where data will be generated in data/
-export expr_name="expr3"
+export expr_name="expr5"
 
 # The names you'd like to give the files of the recorded data
 export srvr_tap_data_file="srvr_tap.pcap"
 export pipe_rcv_data_file="pipe_rcv.csv"
+
+# How long to wait to start the tapped server
+export tap_start_end_delay=1
+# How long to run the experiment for
+export test_time=5
 
 
 # Hosts generation pipe for experiment
@@ -31,9 +36,6 @@ export router_file="./nfra/collection/timestamping-router/recorder.cpp"
 # The object this generates should be called DataRecorder
 #     If it isn't, you need to update recorder.cpp
 export click_collector_object_c_files="./nfra/collection/click-timestamper/timestamprecorder.*"
-
-export tap_start_end_delay=1
-export test_time=5
 
 # \/ Needs to be in agreement with where the click-router is 
 #   outputting its data (so also determined by recorder.cpp)
