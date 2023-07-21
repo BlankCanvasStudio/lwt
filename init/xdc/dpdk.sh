6b83f6a7b65acdee7bc09ef03581027490f80406
adam <spencerstingley6@gmail.com>
2023-07-21 12:11:25 -0500

    Verbosity was poorly written

--- init/xdc/dpdk.sh
+++ init/xdc/dpdk.sh
@@ -6,7 +6,7 @@ for target in "$@"; do
     echo "Copying files over"
     scp ./init/loc/dpdk.sh $target:~/dpdk.sh
     scp ./nfra/bind.sh $target:~/bind.sh
-    echo "Running build"
+    echo "Actually building dpdk"
     ssh $target 'sudo ~/dpdk.sh'
 done
 
