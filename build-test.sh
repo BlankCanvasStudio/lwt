ffbcb9fe2cc01f9278e8e4299aa1a2fcebb3cee9
adam <spencerstingley6@gmail.com>
2023-07-21 12:03:04 -0500

    Make build script more verbose

--- build-test.sh
+++ build-test.sh
@@ -4,26 +4,30 @@ source ./config-test.sh
 
 
 # Build the click router
+echo "Building click router"
 ./init/xdc/fc.sh $click_collector
 ./init/xdc/dpdk.sh $click_collector
+# Move the click router (Data recorder object is moved when 
+#       fastclick is installed so no need to do it here)
+scp $router_file $click_collector:"~/router.cpp"
+
 
 # Make sure the gen files are executable
+echo "Making all gen files executable"
 chmod +x $pipe_gen_exe
 chmod +x $pipe_rcv_exe
 chmod +x $tap_gen_exe
 chmod +x $tap_rcv_exe
 
 # Move gen files over to user_tap and pipe_gen
+echo "Moving files into pipe gen & rcv"
+scp $pipe_rcv_exe $pipe_rcv:"~/rcv"
 scp $pipe_gen_exe $pipe_gen:"~/gen"
-scp $tap_gen_exe $user_tap:"~/gen"
 
 # Move rcv files ove to srvr_tap and pipe_rcv
-scp $pipe_rcv_exe $pipe_rcv:"~/rcv"
+echo "Moving files into tap gen & rcv"
+scp $tap_gen_exe $user_tap:"~/gen"
 scp $tap_rcv_exe $srvr_tap:"~/rcv"
-
-# Move the click router (Data recorder object is moved when 
-#       fastclick is installed so no need to do it here)
-scp $router_file $click_collector:"~/router.cpp"
 
 
 # Bind script is moved when dpdk is set up so no need to do it here
