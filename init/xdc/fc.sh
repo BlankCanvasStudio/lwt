b592e7a0fcd98507f338bf50e7d1162bef26c726
adam <spencerstingley6@gmail.com>
2023-07-21 12:21:55 -0500

    Making sure gcc is installed on build

--- init/xdc/fc.sh
+++ init/xdc/fc.sh
@@ -7,6 +7,8 @@ for target in "$@"; do
     scp ./init/loc/fc.sh $target:~/fastclick.sh
     scp $click_collector_object_c_files $target:~
     scp ./nfra/bind.sh $target:~/bind.sh
+    echo "Installing gcc"
+    sudo apt install gcc
     echo "Actually building fastclick"
     ssh $target 'sudo ~/fastclick.sh'
 done
