f63b96329eac769aace892e8f8b6a7a1819623ce
adam <spencerstingley6@gmail.com>
2023-07-21 12:09:57 -0500

    forgot to put echo

--- init/xdc/fc.sh
+++ init/xdc/fc.sh
@@ -7,7 +7,7 @@ for target in "$@"; do
     scp ./init/loc/fc.sh $target:~/fastclick.sh
     scp $click_collector_object_c_files $target:~
     scp ./nfra/bind.sh $target:~/bind.sh
-    "Running build"
+    echo "Running build"
     ssh $target 'sudo ~/fastclick.sh'
 done
 
